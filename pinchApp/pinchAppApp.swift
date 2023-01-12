0b7b88b6260e4aa01a7722607a7a66839fb03878
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-01-12 11:29:46 +0100

    start point

---
+++ pinchApp/pinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinchAppApp.swift
+//  pinchApp
+//
+//  Created by G BEN on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct pinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
