ed26af81fa89043536727652d99d5eea77473ae4
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-01-13 15:09:36 +0100

    the scalling Effect of image

--- pinchApp/Screen/ContentView.swift
+++ pinchApp/Screen/ContentView.swift
@@ -8,18 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animatedValue:Bool = false
+    @State private var scaleEffectValue:CGFloat = 1
     var body: some View {
         NavigationView{
             ZStack{
-                
+                Image("magazine-front-cover")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    
+                    .scaleEffect(scaleEffectValue)
+                    .gesture(TapGesture(count: 2).onEnded({ Void in
+                        if scaleEffectValue == 1 {
+                            withAnimation(.spring()) {
+                                scaleEffectValue = 5
+                            }
+                        }else{
+                            withAnimation(.spring()) {
+                                scaleEffectValue = 1
+                            }
+                        }
+                    })
+                    )
             }
             .navigationTitle("Pich App")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation {
+                    animatedValue = true
+                }
+            }
         }
         .navigationViewStyle(.stack)
         
         
+        
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
